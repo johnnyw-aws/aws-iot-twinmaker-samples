4db25785b381b268010562713429f8e99de93341
Matt Wild <matwld@amazon.com>
2023-03-08 11:19:04 -0800

    chore(functional test): silence test output
    - silence test logging
    - remove jest config for integration test

--- src/libs/tmdk/jest.config.integration.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-module.exports = {
-  transform: { "^.+\\.ts?$": "ts-jest" },
-  testEnvironment: "node",
-  testRegex: "/integ-tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
