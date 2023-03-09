35b175b49947f95f0e771e745c98b60e598f5b45
Matt Wild <matwld@amazon.com>
2023-03-09 13:10:15 -0800

    fix(tests): skeleton unit tests, implement init tests
    - add skeleton test cases for each implemented command
    - implement basic tests for init

--- src/libs/tmdk/tests/init.test.ts
+++ src/libs/tmdk/tests/init.test.ts
@@ -3,20 +3,58 @@
 
 import { handler, Options } from "../src/commands/init";
 import { Arguments } from "yargs";
-import { ResourceNotFoundException } from "@aws-sdk/client-iottwinmaker";
-import { createDirectory, mockExit } from "./test-utils";
+import {
+  GetComponentTypeCommand,
+  GetEntityCommand,
+  GetWorkspaceCommand,
+  IoTTwinMakerClient,
+  ListComponentTypesCommand,
+  ListEntitiesCommand,
+  ListScenesCommand,
+  ResourceNotFoundException,
+} from "@aws-sdk/client-iottwinmaker";
+import {
+  createDirectory,
+  emptyListComponentTypesResp,
+  emptyListEntitiesResp,
+  basicWorkspaceId,
+  emptyListScenesResp,
+  oneCtListComponentTypesResp,
+  getComponentType1Resp,
+  expectComponentType1,
+  oneEntityListEntitiesResp,
+  getEntity1Resp,
+  expectEntity1,
+  oneSceneListScenesResp,
+  localResourcesDir, scene1,
+} from "./test-utils";
+import { mockClient } from "aws-sdk-client-mock";
+import { GetObjectCommand, GetObjectCommandOutput, S3Client } from "@aws-sdk/client-s3";
+import * as path from 'path';
+import { ReadStream } from 'fs';
+import { Readable } from 'stream';
+const fs = require("fs");
 
 const outDir = "/tmp/init-unit-tests";
 beforeAll(() => {
   createDirectory(outDir);
 });
 
-jest.mock("../src/lib/aws-clients");
+const twinmakerMock = mockClient(IoTTwinMakerClient);
+const s3Mock = mockClient(S3Client);
 
 describe("testing init", () => {
-  beforeEach(mockExit.mockClear);
+  beforeEach(() => {
+    s3Mock.reset();
+    twinmakerMock.reset();
+    jest.spyOn(fs, "writeFileSync");
+  });
 
-  test("init_givenNoWorkspace_expectError", async () => {
+  test("init_givenWorkspaceDoesNotExist_expectError", async () => {
+    twinmakerMock
+      .on(GetWorkspaceCommand)
+      .rejects(new ResourceNotFoundException({ $metadata: {}, message: "" }));
+
     const argv2 = {
       _: ["init"],
       $0: "tmdk_local",
@@ -27,27 +65,227 @@ describe("testing init", () => {
     await expect(handler(argv2)).rejects.toThrow(ResourceNotFoundException);
   });
 
-  test("init_givenWorkspaceDoesNotExist_expectEmptyProject", async () => {
-    // TODO
+  test("init_givenNoResources_expectEmptyTmdk", async () => {
+    twinmakerMock.on(GetWorkspaceCommand).resolves({});
+    twinmakerMock
+      .on(ListComponentTypesCommand)
+      .resolves(emptyListComponentTypesResp);
+    twinmakerMock.on(ListEntitiesCommand).resolves(emptyListEntitiesResp);
+    twinmakerMock.on(ListScenesCommand).resolves(emptyListScenesResp);
+
+    const argv2 = {
+      _: ["init"],
+      $0: "tmdk_local",
+      region: "us-east-1",
+      "workspace-id": basicWorkspaceId,
+      out: outDir,
+    } as Arguments<Options>;
+    expect(await handler(argv2)).toBe(0);
+    const expectedTmdk: any = {
+      version: "0.0.2",
+      "component-types": [],
+      scenes: [],
+      models: [],
+      entities: "entities.json",
+    };
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/tmdk.json`,
+      JSON.stringify(expectedTmdk, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/entities.json`,
+      JSON.stringify([], null, 4)
+    );
   });
 
-  test("init_givenNoResources_expectJustWorkspace", async () => {
-    // TODO
+  test("init_given1ComponentType_expectSuccess", async () => {
+    twinmakerMock.on(GetWorkspaceCommand).resolves({});
+    twinmakerMock
+      .on(ListComponentTypesCommand)
+      .resolves(oneCtListComponentTypesResp);
+    twinmakerMock.on(GetComponentTypeCommand).resolves(getComponentType1Resp);
+    twinmakerMock.on(ListEntitiesCommand).resolves(emptyListEntitiesResp);
+    twinmakerMock.on(ListScenesCommand).resolves(emptyListScenesResp);
+
+    const argv2 = {
+      _: ["init"],
+      $0: "tmdk_local",
+      region: "us-east-1",
+      "workspace-id": basicWorkspaceId,
+      out: outDir,
+    } as Arguments<Options>;
+    expect(await handler(argv2)).toBe(0);
+    const expectedTmdk = {
+      version: "0.0.2",
+      "component-types": [`${getComponentType1Resp["componentTypeId"]}.json`],
+      scenes: [],
+      models: [],
+      entities: "entities.json",
+    };
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/tmdk.json`,
+      JSON.stringify(expectedTmdk, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/entities.json`,
+      JSON.stringify([], null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/${getComponentType1Resp["componentTypeId"]}.json`,
+      JSON.stringify(expectComponentType1, null, 4)
+    );
   });
 
-  test("init_givenJustComponentTypes_expectSuccess", async () => {
-    // TODO
-  });
+  test("init_given1SceneAnd1Model_expectSuccess", async () => {
+    twinmakerMock.on(GetWorkspaceCommand).resolves({});
+    twinmakerMock
+      .on(ListComponentTypesCommand)
+      .resolves(emptyListComponentTypesResp);
+    twinmakerMock.on(ListEntitiesCommand).resolves(emptyListEntitiesResp);
+    twinmakerMock.on(ListScenesCommand).resolves(oneSceneListScenesResp);
+    s3Mock
+      .on(GetObjectCommand, { Bucket: "workspace-bucket", Key: "scene1.json" })
+      .resolves({
+        $metadata: {},
+        Body: { transformToString: (encoding?: string) =>
+          {return Promise.resolve(JSON.stringify(scene1, null, 4));} } as any,
+      });
+    s3Mock
+      .on(GetObjectCommand, { Bucket: "workspace-bucket", Key: "model1.glb" })
+      .resolves({
+        $metadata: {},
+        Body: { transformToString: (encoding?: string) =>
+          {return Promise.resolve(fs.readFileSync(path.join(localResourcesDir, "model1.glb")));} } as any,
+      });
 
-  test("init_givenJustScenes_expectSuccess", async () => {
-    // TODO
+    const argv2 = {
+      _: ["init"],
+      $0: "tmdk_local",
+      region: "us-east-1",
+      "workspace-id": basicWorkspaceId,
+      out: outDir,
+    } as Arguments<Options>;
+    expect(await handler(argv2)).toBe(0);
+    const expectedTmdk = {
+      version: "0.0.2",
+      "component-types": [],
+      scenes: ["scene1.json"],
+      models: ["model1.glb"],
+      entities: "entities.json",
+    };
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/tmdk.json`,
+      JSON.stringify(expectedTmdk, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/entities.json`,
+      JSON.stringify([], null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/scene1.json`,
+      JSON.stringify(scene1, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/3d_models/model1.glb`,
+      fs.readFileSync(path.join(localResourcesDir, "model1.glb")),
+    );
   });
 
   test("init_givenJustEntities_expectSuccess", async () => {
-    // TODO
+    twinmakerMock.on(GetWorkspaceCommand).resolves({});
+    twinmakerMock
+      .on(ListComponentTypesCommand)
+      .resolves(emptyListComponentTypesResp);
+    twinmakerMock.on(ListEntitiesCommand).resolves(oneEntityListEntitiesResp);
+    twinmakerMock.on(GetEntityCommand).resolves(getEntity1Resp);
+    twinmakerMock.on(ListScenesCommand).resolves(emptyListScenesResp);
+
+    const argv2 = {
+      _: ["init"],
+      $0: "tmdk_local",
+      region: "us-east-1",
+      "workspace-id": basicWorkspaceId,
+      out: outDir,
+    } as Arguments<Options>;
+    expect(await handler(argv2)).toBe(0);
+    const expectedTmdk = {
+      version: "0.0.2",
+      "component-types": [],
+      scenes: [],
+      models: [],
+      entities: "entities.json",
+    };
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/tmdk.json`,
+      JSON.stringify(expectedTmdk, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/entities.json`,
+      JSON.stringify([expectEntity1], null, 4)
+    );
   });
 
   test("init_givenFullWorkspace_expectSuccess", async () => {
-    // TODO
+    twinmakerMock.on(GetWorkspaceCommand).resolves({});
+    twinmakerMock
+      .on(ListComponentTypesCommand)
+      .resolves(oneCtListComponentTypesResp);
+    twinmakerMock.on(GetComponentTypeCommand).resolves(getComponentType1Resp);
+    twinmakerMock.on(ListEntitiesCommand).resolves(oneEntityListEntitiesResp);
+    twinmakerMock.on(GetEntityCommand).resolves(getEntity1Resp);
+    twinmakerMock.on(ListScenesCommand).resolves(oneSceneListScenesResp);
+    s3Mock
+      .on(GetObjectCommand, { Bucket: "workspace-bucket", Key: "scene1.json" })
+      .resolves({
+        $metadata: {},
+        Body: { transformToString: (encoding?: string) =>
+          {return Promise.resolve(JSON.stringify(scene1, null, 4));} } as any,
+      });
+    s3Mock
+      .on(GetObjectCommand, { Bucket: "workspace-bucket", Key: "model1.glb" })
+      .resolves({
+        $metadata: {},
+        Body: { transformToString: (encoding?: string) =>
+          {return Promise.resolve(fs.readFileSync(path.join(localResourcesDir, "model1.glb")));} } as any,
+      });
+
+    const argv2 = {
+      _: ["init"],
+      $0: "tmdk_local",
+      region: "us-east-1",
+      "workspace-id": basicWorkspaceId,
+      out: outDir,
+    } as Arguments<Options>;
+    expect(await handler(argv2)).toBe(0);
+    const expectedTmdk = {
+      version: "0.0.2",
+      "component-types": [`${getComponentType1Resp["componentTypeId"]}.json`],
+      scenes: ["scene1.json"],
+      models: ["model1.glb"],
+      entities: "entities.json",
+    };
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/tmdk.json`,
+      JSON.stringify(expectedTmdk, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/entities.json`,
+      JSON.stringify([expectEntity1], null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/${getComponentType1Resp["componentTypeId"]}.json`,
+      JSON.stringify(expectComponentType1, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/scene1.json`,
+      JSON.stringify(scene1, null, 4)
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      `${outDir}/3d_models/model1.glb`,
+      fs.readFileSync(path.join(localResourcesDir, "model1.glb")),
+    );
   });
 });
--- src/libs/tmdk/tests/test-utils.ts
+++ src/libs/tmdk/tests/test-utils.ts
@@ -2,6 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as fs from "fs";
+import {
+  GetComponentTypeCommandOutput,
+  GetEntityCommandOutput,
+  ListComponentTypesCommandOutput,
+  ListEntitiesCommandOutput,
+  ListScenesCommandOutput,
+} from "@aws-sdk/client-iottwinmaker";
+import * as path from 'path';
+
+export const localResourcesDir = path.join(
+  __dirname,
+  "test-resources"
+);
+
+export const basicWorkspaceId = "workspaceId";
 
 export const mockExit = jest
   .spyOn(process, "exit")
@@ -14,3 +29,170 @@ export function createDirectory(dir: string): void {
     fs.mkdirSync(dir);
   }
 }
+
+// TODO refactor to separate file
+
+export const emptyListComponentTypesResp: ListComponentTypesCommandOutput = {
+  $metadata: {},
+  componentTypeSummaries: [],
+  maxResults: 0,
+  nextToken: undefined,
+  workspaceId: basicWorkspaceId,
+};
+
+export const emptyListEntitiesResp: ListEntitiesCommandOutput = {
+  $metadata: {},
+  entitySummaries: [],
+  nextToken: undefined,
+};
+
+export const emptyListScenesResp: ListScenesCommandOutput = {
+  $metadata: {},
+  sceneSummaries: [],
+  nextToken: undefined,
+};
+
+export const getComponentType1Resp: GetComponentTypeCommandOutput = {
+  $metadata: {},
+  workspaceId: basicWorkspaceId,
+  arn: "componentType1Arn",
+  componentTypeId: "componentType1",
+  componentTypeName: "componentType1Name",
+  creationDateTime: new Date(),
+  description: "description",
+  status: {
+    error: {
+      code: "",
+      message: "",
+    },
+    state: "",
+  },
+  updateDateTime: new Date(),
+};
+
+export const oneCtListComponentTypesResp: ListComponentTypesCommandOutput = {
+  $metadata: {},
+  componentTypeSummaries: [
+    {
+      arn: getComponentType1Resp.arn,
+      componentTypeId: getComponentType1Resp.componentTypeId,
+      componentTypeName: getComponentType1Resp.componentTypeName,
+      creationDateTime: getComponentType1Resp.creationDateTime,
+      description: getComponentType1Resp.description,
+      status: {
+        error: {
+          code: "",
+          message: "",
+        },
+        state: "",
+      },
+      updateDateTime: getComponentType1Resp.updateDateTime,
+    },
+  ],
+  maxResults: 0,
+  nextToken: undefined,
+  workspaceId: basicWorkspaceId,
+};
+
+export const expectComponentType1 = {
+  componentTypeId: getComponentType1Resp.componentTypeId,
+  description: getComponentType1Resp.description,
+  extendsFrom: getComponentType1Resp.extendsFrom,
+  functions: getComponentType1Resp.functions,
+  isSingleton: getComponentType1Resp.isSingleton,
+  propertyDefinitions: getComponentType1Resp.propertyDefinitions,
+};
+
+export const getEntity1Resp: GetEntityCommandOutput = {
+  $metadata: {},
+  hasChildEntities: false,
+  parentEntityId: "$ROOT",
+  workspaceId: basicWorkspaceId,
+  arn: "componentType1Arn",
+  entityId: "entity1",
+  entityName: "entity1Name",
+  creationDateTime: new Date(),
+  description: "description",
+  status: {
+    error: {
+      code: "",
+      message: "",
+    },
+    state: "",
+  },
+  updateDateTime: new Date(),
+};
+
+export const oneEntityListEntitiesResp: ListEntitiesCommandOutput = {
+  $metadata: {},
+  entitySummaries: [
+    {
+      arn: getEntity1Resp.arn,
+      entityId: getEntity1Resp.entityId,
+      entityName: getEntity1Resp.entityName,
+      creationDateTime: getEntity1Resp.creationDateTime,
+      description: getEntity1Resp.description,
+      status: {
+        error: {
+          code: "",
+          message: "",
+        },
+        state: "",
+      },
+      updateDateTime: getEntity1Resp.updateDateTime,
+    },
+  ],
+  nextToken: undefined,
+};
+
+export const expectEntity1 = {
+  components: [],
+  description: getEntity1Resp.description,
+  entityId: getEntity1Resp.entityId,
+  entityName: getEntity1Resp.entityName,
+  parentEntityId: getEntity1Resp.parentEntityId,
+};
+
+export const scene1 = {
+  specVersion: "1.0",
+  version: "1",
+  unit: "meters",
+  properties: {},
+  nodes: [
+    {
+      name: "model1",
+      transform: {
+        position: [0, 0, 0],
+        rotation: [0, 0, 0],
+        scale: [1, 1, 1],
+      },
+      transformConstraint: {},
+      components: [
+        {
+          type: "ModelRef",
+          uri: "model1.glb",
+          modelType: "GLB",
+        },
+      ],
+      properties: {},
+    },
+  ],
+  rootNodeIndexes: [0],
+  cameras: [],
+  rules: {},
+};
+
+export const oneSceneListScenesResp: ListScenesCommandOutput = {
+  $metadata: {},
+  sceneSummaries: [
+    {
+      sceneId: "scene1",
+      contentLocation: "s3://workspace-bucket/scene1.json",
+      arn: "scene1Arn",
+      creationDateTime: new Date(),
+      updateDateTime: new Date(),
+      description: "description",
+    },
+  ],
+  nextToken: undefined,
+};
