bad552c9887ede8ecdf38aecee85264cdd6ac521
Matt Wild <matwld@amazon.com>
2023-03-13 11:07:20 -0700

    chore(configuration): update configuration to get closer to appkit
    - change ts and jest configuration to align closer to appkit
    - resolve isolatedModules issue in component-type and entity

--- src/libs/tmdk/jest.config.js
+++ src/libs/tmdk/jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
   testEnvironment: "node",
   testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
