e17e02a369299306d18b662597d819cb6e751684
johnnyw-aws <johnnyw-aws@users.noreply.github.com>
2023-04-27 16:39:11 -0700

    revert dev port

--- src/workspaces/cookiefactoryv2/CookieFactoryDemo/webpack.dev.js
+++ src/workspaces/cookiefactoryv2/CookieFactoryDemo/webpack.dev.js
@@ -1,5 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 2023
-// SPDX-License-Identifier: Apache-2.0
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { merge } from 'webpack-merge';
@@ -14,7 +12,7 @@ export default merge(common, {
 
   devServer: {
     hot: true,
-    port: 9000,
+    port: 5000,
     static: './public/'
   },
 
