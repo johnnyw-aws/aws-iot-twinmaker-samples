6185d2d52c32b1e474e3ac777d5c5bcc60cdfd8e
chrisbol <chrisbol@amazon.com>
2023-05-02 14:32:44 +0000

    Create sites.ts template, update README

--- src/workspaces/cookiefactoryv2/CookieFactoryDemo/src/config/users.template.ts
+++ src/workspaces/cookiefactoryv2/CookieFactoryDemo/src/config/users.template.ts
@@ -1,20 +1,23 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 2023
 // SPDX-License-Identifier: Apache-2.0
-import type { UserConfig } from '@/lib/types';
 
 /**
  * Authenticated user configuration.
  * RENAME THIS TEMPLATE TO `users.ts`
+ */
+import type { UserConfig } from '@/lib/types';
+
+/**
  * @proprety `email` AWS Cognito user account credential
  * @proprety `password` AWS Cognito user account credential
  */
 const users: UserConfig[] = [
   {
-    email: '',
-    firstName: '',
-    lastName: '',
-    title: '',
-    password: ''
+    email: '__FILL_IN__',
+    firstName: '__FILL_IN__',
+    lastName: '__FILL_IN__',
+    password: '__FILL_IN__',
+    title: '__FILL_IN__'
   }
 ];
 
